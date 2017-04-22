91c0365fda3bc6ad7867ad861d3203829af0a783
jwalsh370 <jwalsh370@gmail.com>
2017-04-22 13:40:31 -0700

    fix bug in doctor.js, call correct responses.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,25 +1,31 @@
 var doctor = require('./../js/doctor.js').doctorModule;
-var getDoctors = require('./../js/doctor.js').getDoctors;
+var mapApiKey = require('./../.env').mapApiKey;
+
+var currentDoctorObject = new Doctor();
 
 var doctorsDisplayed = function(doctors) {
   doctors.forEach(function(doctor) {
     $('#doctors').append('<tr class="info">'+
-                          "<td>doctor.data.practices.name</td>"+
-                          "<td>doctor.data.profile.first_name, doctor.data.profile.last_name, doctor.data.profile.title</td>"+
-                          "<td>doctor.data.profile.gender</td>"+
-                          "<td>doctor.data.practices.phones.number</td>"+
-                          "<td>doctor.data.website</td>"+
-                          "<td>doctor.data.visit_address</td>"+
+                          "<td>doctor.practices.name</td>"+
+                          "<td>doctor.profile.first_name, doctor.profile.last_name</td>"+
+                          "<td>doctor.profile.gender</td>"+
+                          "<td>doctor.practices.phones.number</td>"+
+                          "<td>doctor.website</td>"+
+                          "<td>doctor.visit_address</td>"+
                         '</tr>');
   });
 };
 
 $(document).ready(function(){
-  var currentDoctorObject = new Doctor();
-
   $('#doctorSearch').submit(function(){
     event.preventDefault();
+    var issue = $('#issue').val();
+    var zip = parseInt($('#zip').val());
+    var distance = parseInt($('#distance').val());
+    $('#zip').val('');
+    $('#distance').val('');
+    $('#issue').val('');
 
-    currentDoctorObject.getDoctors(medicalIssue);
+    currentDoctorObject.getDoctors(allDoctors, zip, distance, issue);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,38 +1,23 @@
-var apiKey = require('./../.env').apiKey;
+var apiKey = require('../.env').apiKey;
 
 function Doctor() {
 }
 
-Doctor.prototype.findDoctorIssue = function (issue, location) {
-  return $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + issue + '&location=' + location + '%2C100&user_location=' + location + '&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
-    return DoctorResponse(response);
+Doctor.prototype.findDoctorIssue = function(zip, distance, issue, allDoctors) {
+   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + issue + '&location=' + location + '%2C100&user_location=' + location + '&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
+    var allDoctors = [];
+    response.doctors.forEach(function(doctor){
+      var newDoctor = {};
+      newDoctor.name = doctor.first_name + " " + doctor.last_name;
+      newDoctor.specialties = doctor.specialties;
+      newDoctor.education = doctor.educations;
+        allDoctors.push(newDoctor);
+      newDoctor
+    })
   }).fail(function(error) {
   });
 };
 
-  var DoctorResponse = function (response) {
-        var allDoctors = [];
-        response.data.forEach(doctor => {
-          var newDoctor = {};
-          newDoctor.name = doctor.profile.first_name + " " + doctor.profile.last_name;
-          newDoctor.title = doctor.profile.title;
-          // newDoctor.img = doctor.profile.image_url;
-          newDoctor.bio = doctor.profile.bio;
-          newDoctor.specialties = doctor.specialties;
-          newDoctor.education = doctor.educations;
-          // newDoctor.practices = doctor.practices.map(practice => {
-          //   var prac = {};
-          //   prac.name = practice.name;
-          //   prac.newPatients = practice.accepts_new_patients;
-          //   prac.location = {lat: practice.lat, lng: practice.lon};
-          //   prac.distance = practice.distance;
-          //   prac.address = practice.visit_address;
-          //   prac.phone = practice.phones;
-          //   return prac;
-          });
-          allDoctors.push(newDoctor);
-        })
-        return allDoctors;
-      };
+
 
 exports.doctorModule = Doctor;
