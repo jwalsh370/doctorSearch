dba15577cbb5ae00c2841c490529226479167b15
jwalsh370 <jwalsh370@gmail.com>
2017-04-22 10:35:31 -0700

    add .gitignre file

---
+++ js/doctor-interface.js
@@ -0,0 +1,28 @@
+var doctor = require('./../js/doctor.js').doctorModule;
+var getDoctors = require('./../js/doctor.js').getDoctors;
+
+var doctorsDisplayed = function(doctors) {
+  doctors.forEach(function(doctor) {
+    $('#doctors').append('<tr class="info">'+
+                          "<td>doctor.data.practices.name</td>"+
+                          "<td>doctor.data.profile.first_name, doctor.data.profile.last_name, doctor.data.profile.title</td>"+
+                          "<td>doctor.data.profile.gender</td>"+
+                          "<td>doctor.data.practices.phones.number</td>"+
+                          "<td>doctor.data.website</td>"+
+                          "<td>doctor.data.visit_address</td>"+
+                        '</tr>');
+  });
+};
+
+$(document).ready(function(){
+  var currentDoctorObject = new Doctor();
+
+  $('#doctorSearch').submit(function(){
+    event.preventDefault();
+
+    var medicalIssue = $('#medicalIssue').val();
+    $('#medicalIssue').val('');
+
+    currentDoctorObject.getDoctors(medicalIssue);
+  });
+});
---
+++ js/doctor.js
@@ -0,0 +1,28 @@
+var apiKey = require('./../.env').apiKey;
+
+function Doctor() {
+}
+
+exports.getDoctors = function(medicalIssue, searchResults) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key='+apiKey)
+    .then(function(result) {
+      console.log(result);
+      var allDoctors=[];
+      result.data.forEach(function(doctor) {
+        var individualDoctor = {};
+
+        individualDoctor.practiceName = data.practices.name;
+        individualDoctor.phone = data.practices.phones.number;
+        individualDoctor.website = doctor.website;
+        individualDoctor.office = doctor.name;
+        individualDoctor.address = doctor.visit_address;
+
+        allDoctors.push(individualDoctor);
+    })
+    .fail(function(error){
+      $('#error').text(error.responseJSON.message);
+    });
+  });
+};
+
+exports.doctorModule = Doctor;
