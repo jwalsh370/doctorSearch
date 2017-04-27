347622696fbf2c29b181c38d77efcb6adc68f1ea
jwalsh370 <jwalsh370@gmail.com>
2017-04-26 18:50:06 -0700

    cleaned up doctor.js, remove erros, 401 bug still exists.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,24 +1,21 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 
-var currentDoctorObject = new Doctor();
+
 
 var displayDoctors = function(doctors) {
   doctors.forEach(function(doctor) {
     $('#doctors').append('<tr class="info">'+
-                          "<td>Doctor.practices.name</td>"+
-                          "<td>Doctor.profile.first_name, Doctor.profile.last_name</td>"+
-                          "<td>Doctor.profile.gender</td>"+
-                          "<td>Doctor.practices.phones.number</td>"+
-                          "<td>Doctor.website</td>"+
-                          "<td>Doctor.visit_address</td>"+
-                          "<td>Doctor.specialties</td>"+
-                          "<td>Doctor.educations"+
-                        '</tr>');
+                            "<td>" + doctor.name + "</td>"+
+                            "<td>" + doctor.specialties + "</td>"+
+                            "<td>" + doctor.educations + "</td>" +
+                           '</tr>'
+                        );
   });
 };
 
 $(document).ready(function(){
+  var currentDoctorObject = new Doctor();
   $('#allDoctors').submit(function(){
     event.preventDefault();
     var issue = $('#issue').val();
--- js/doctor.js
+++ js/doctor.js
@@ -1,23 +1,28 @@
+
+var apiUrl = "https://api.betterdoctor.com/2016-03-01/doctors?query=";
+var apiLocation = "&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=";
 var apiKey = require('./../.env').apiKey;
-var apiUrl = 'https://api.betterdoctor.com/2016-03-01/doctors?query=';
-var apiLocation = '&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=';
 
 
 function Doctor() {
 }
 
-Doctor.prototype.getDoctors = function( issue, displayDoctors) {
-   $.get(apiUrl + issue + apiLocation + apiKey).then(function(response) {
-    var allDoctors = [];
-    response.doctors.forEach(function(doctor){
+Doctor.prototype.getDoctors = function(issue, displayDoctors) {
+   $.get(apiUrl + issue + apiLocation + apiKey)
+    .then(function(response) {
+      var allDoctors = [];
+      response.data.forEach(function(doctor) {
       var newDoctor = {};
-      newDoctor.name = doctor.first_name + " " + doctor.last_name;
-      newDoctor.specialties = doctor.specialties;
-      newDoctor.education = doctor.educations;
+      newDoctor.name = doctor.profile.first_name + " " + doctor.profile.last_name;
+      newDoctor.specialties = doctor.profile.specialties;
+      newDoctor.education = doctor.profile.educations;
         allDoctors.push(newDoctor);
     });
     displayDoctors(allDoctors);
-  }).fail(function(error) {
+  })
+
+  .fail(function(error) {
+     $('#error').text(error.responseJSON.message);
   });
 };
 
